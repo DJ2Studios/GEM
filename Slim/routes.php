ea495448a7d2b581a9674d4a280be69d292ce84c
Justin Suh <jsuh@smu.edu>
2016-05-04 20:56:27 -0500

    Update routes.php

--- Slim/routes.php
+++ Slim/routes.php
@@ -1,8 +1,8 @@
 <?php
 // Routes
 
-//require 'vendor/autoload.php';
-//$app = new \Slim\Slim();
+require 'vendor/autoload.php';
+$app = new \Slim\Slim();
 
 //$app->get('/[{name}]', function ($request, $response, $args) 
 $app->get('/hello', function ($request, $response, $args) {
